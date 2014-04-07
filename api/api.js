0a399ac88e43618b6d30d54883a015aaa7417e5b
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-04-06 23:04:03 -0400

    initial commit

---
+++ api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "d3adaptor"
+    ],
+    "modules": [
+        "cola"
+    ],
+    "allModules": [
+        {
+            "displayName": "cola",
+            "name": "cola"
+        }
+    ]
+} };
+});
